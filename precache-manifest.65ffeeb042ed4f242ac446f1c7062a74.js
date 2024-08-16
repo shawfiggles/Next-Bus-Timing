c1de0b2c4f86ccadc24fb06b275d75e0829a6c8b
shawfiggles <shafeeqhisyam@gmail.com>
2024-08-16 11:59:29 +0400

    Updates

---
+++ precache-manifest.65ffeeb042ed4f242ac446f1c7062a74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e5b28af867ee645e8598635c6c3fa15",
+    "url": "/Next-Bus-Timing/index.html"
+  },
+  {
+    "revision": "60ca6ff5cf34c4d5e973",
+    "url": "/Next-Bus-Timing/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a786c008b3fdf75a6b2e",
+    "url": "/Next-Bus-Timing/static/js/2.ee643914.chunk.js"
+  },
+  {
+    "revision": "73b4d1c9ae82f626d5309506f76c79ff",
+    "url": "/Next-Bus-Timing/static/js/2.ee643914.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60ca6ff5cf34c4d5e973",
+    "url": "/Next-Bus-Timing/static/js/main.cd1165ab.chunk.js"
+  },
+  {
+    "revision": "872e40489af18b8a4f28",
+    "url": "/Next-Bus-Timing/static/js/runtime-main.378158da.js"
+  }
+]);
