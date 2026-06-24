afca02d769b73205a6447349c8e880be48174dae
Fignox <shawfiggles@users.noreply.github.com>
2026-06-24 15:35:59 +0000

    Fix Netlify npm peer dependency install

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders bus schedule heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/bus schedule/i);
+  expect(headingElement).toBeInTheDocument();
 });
